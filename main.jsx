fd8bccd498504ad27a66398b549f4406a65ec9f8
painel-roleta <lucivansilva401@gmail.com>
2025-07-09 12:08:41 -0500

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,11 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import PainelRoleta from "./PainelRoleta";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PainelRoleta />
+  </React.StrictMode>
+);
